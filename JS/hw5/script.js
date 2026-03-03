28ffa94f3136eda3cbe7022a186e712a4c4c05b5
Dana Assylkhan <danaassylkhan@MacBook-Air-Dana.local>
2026-03-03 17:44:08 +0500

    Add responsive testimonials carousel with Swiper

---
+++ JS/hw5/script.js
@@ -0,0 +1,34 @@
+new Swiper(".testimonials-swiper", {
+  slidesPerView: 1,
+  spaceBetween: 16,
+  loop: true,
+  grabCursor: true,
+  simulateTouch: true,
+  allowTouchMove: true,
+  watchOverflow: false,
+  speed: 600,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  keyboard: {
+    enabled: true,
+  },
+  mousewheel: {
+    forceToAxis: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1.25,
+      spaceBetween: 18,
+    },
+    900: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 24,
+    },
+  },
+});
